62eeb55701b43e0c201c3719ae1b2ce35352be1c
Justin Barclay <justincbarclay@gmail.com>
2022-05-02 16:14:31 +0100

    Finish chapter 7

--- chapter-7/src/main.rs
+++ chapter-7/src/main.rs
@@ -1,25 +1,31 @@
+use std::fs::File;
 use std::io::prelude::*;
+use std::env;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8] = br#"
-fn main() {
-    println!("Hello, world!");
-}"#;
 
 fn main() -> std::io::Result<()> {
-  let mut buffer: Vec<u8> = vec![];
+  let arg1 = env::args().nth(1);
 
-  INPUT.read_to_end(&mut buffer)?;
+  let fname = arg1.expect("usage: fview FILENAME");
 
-  let mut position_in_input = 0;
+  let mut f = File::open(&fname).expect("Unable to open file.");
+  let mut pos = 0;
+  let mut buffer = [0; BYTES_PER_LINE];
 
-  for line in buffer.chunks(BYTES_PER_LINE) {
-    print!("[0x{:08x}] ", position_in_input);
-    for byte in line {
-      print!("{:02x}", byte);
+  while let Ok(_) = f.read_exact(&mut buffer){
+    print!("[0x{:08x}] ", pos);
+
+    for byte in &buffer {
+      match *byte {
+        0x00 => print!(".  "),
+        0xff => print!("## "),
+        _ => print!("{:02x}", byte)
+      }
     }
-    println!();
-    position_in_input += BYTES_PER_LINE;
+
+    println!("");
+    pos += BYTES_PER_LINE;
   }
   Ok(())
 }
